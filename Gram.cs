f99f4179db87a9e6114115e7384674557cdb2f12
HalimaGhannoum <lily.hl80@gmail.com>
2021-07-15 20:23:48 +0300

    fm

---
+++ Gram.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace Start
+{
+    public partial class cours_de_grammaire : Form
+    {
+       public static  int dest;
+        Random r = new Random();
+        binding binding2 = new binding();
+        XmlDocument gram;
+     
+        public bool cours, gram2=false;
+        public static int[] scores = new int[10];
+        public cours_de_grammaire()
+        {
+            InitializeComponent();
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gram = new XmlDocument(); 
+            dr = Variables.XmlReader(Application.StartupPath + "\\users.xml");
+            gram.Load(Application.StartupPath + @"\Gramm.xml");
+            if (gram2) panel2.Visible = true;
+            if (!cours) 
+            {  
+                button11.Click += lvl1_grams
+                    ; button3.Click += lvl1_grams; 
+                button4.Click += lvl1_grams; 
+                button7.Click += lvl1_grams; 
+                button6.Click += lvl1_grams; 
+                button9.Click += lvl1_grams;
+            }
+            CryptageEtHachage.DeCrypNode(gram.DocumentElement);
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            
+            panconv = panel2; if (!cours) pictureBox1.Visible = true;
+            else
+            {
+                Cours cf = new Cours(); cf.conjugaison = false;
+
+                cf.debut = 31;cf.fin = 33;cf.Show();
+        }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        { panconv = panel1; if (!cours) pictureBox1.Visible = true;
+            else
+            {
+                Cours cf = new Cours(); cf.conjugaison = false;
+
+                cf.debut = 27; cf.fin = 29; cf.Show();
+
+            }
+           
+        }
+
+    
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            panconv = panel1;if(!cours) pictureBox1.Visible = true;
+            else
+            {
+                Cours cf = new Cours(); cf.conjugaison = false;
+
+                cf.debut = 17; cf.fin = 19; cf.Show();
+
+            }
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panconv = panel1;if(!cours) pictureBox1.Visible = true;
+            else
+            {
+                Cours cf = new Cours(); cf.conjugaison = false;
+                cf.debut = 15; cf.fin = 17; cf.Show();
+
+                
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            panconv = panel1;if(!cours) pictureBox1.Visible = true;
+            else
+            {
+               
+                Cours cf = new Cours();cf.conjugaison = false;
+                cf.debut = 22; cf.fin = 27; cf.Show();
+            }
+        }
+        private void lvl1_grams(object sender, EventArgs e)
+        {
+            Button lb = (Button)sender; binding2.Dispose();
+
+            panel1.Hide(); panel2.Visible = false;
+            int a = int.Parse(lb.Tag.ToString().Split(',')[0]), b = int.Parse(lb.Tag.ToString().Split(',')[1]);
+            binding2 = new binding(lb.Tag.ToString().Split(',')[5], int.Parse(lb.Tag.ToString().Split(',')[3]), int.Parse(lb.Tag.ToString().Split(',')[2]), panconv, ComMethodes.Generate(a, b)); userconv = binding2;
+            //binding2.rand = ComMethodes.Generate(a, b); binding2.dest = int.Parse(lb.Tag.ToString().Split(',')[3]);
+            //binding2.depart = int.Parse(lb.Tag.ToString().Split(',')[2]); binding2.lecon = lb.Tag.ToString().Split(',')[5]; binding2.Location = new Point(200, 150);binding2.panl = panconv;
+            binding2.Location = new Point(200, 150); this.Controls.Add(binding2);
+            userconv.Visible = true;
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            panconv = panel1;if (!cours)
+            {
+                pictureBox1.Visible = true;
+                panel1.Hide(); panel2.Hide();
+                encadrerEntourer enen = new encadrerEntourer(); userconv = enen; userconv.Visible = true; enen.Dock = DockStyle.Fill; this.Controls.Add(enen);
+            }
+            else
+            {
+                Cours cf = new Cours(); cf.conjugaison = false;
+                cf.debut = 20; cf.fin = 22; cf.Show();
+            }
+        }
+        Panel panconv;
+        private void button10_Click(object sender, EventArgs e)
+        {
+            panconv = panel2;
+            if (!cours)
+            {
+                panel1.Hide(); panel2.Hide();
+                pictureBox1.Visible = true; 
+                panel1.Hide(); panel2.Hide(); phraseoEx ep = new phraseoEx(); userconv = ep; userconv.Visible = true; this.Controls.Add(ep); ep.Dock = DockStyle.Fill;
+                ep.Show(); pictureBox1.BringToFront();
+            }
+            else
+            {
+                Cours cf = new Cours(); cf.conjugaison = false;
+                cf.debut = 1;cf.fin = 5;cf.Show();
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            panconv = panel2;if(!cours) pictureBox1.Visible = true;
+            else
+            {
+                Cours cf = new Cours();cf.conjugaison = false;
+                cf.debut = 5; cf.fin = 7;cf.Show();
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            panconv = panel2; 
+            panel1.Hide();
+            panel2.Hide();
+            if (!cours)
+            {
+                pictureBox1.Visible = true; 
+                choixMultiple choiprn = new choixMultiple(ComMethodes.Generate(6, 10), "Posses", panconv, 6, 10, "Francais");
+               
+
+                userconv = choiprn;
+                userconv.Visible = true;
+                choiprn.Show();
+                choiprn.BringToFront();
+                this.Controls.Add(choiprn);
+                choiprn.Dock = DockStyle.Fill;
+                pictureBox1.BringToFront();
+            }
+            else
+            {
+                Cours cf = new Cours();
+                cf.conjugaison = false;
+                cf.debut = 29;
+                cf.fin = 31;
+                cf.Show();
+
+            }
+        }
+       UserControl  userconv;
+        private void button14_Click(object sender, EventArgs e)
+        {
+            panconv = panel1;
+            panel1.Hide();
+            panel2.Hide();
+            if (!cours) 
+            {
+               
+                choixMultiple choiADj = new choixMultiple(ComMethodes.Generate(7, 10), "Adjectifs", panconv, 7, 10, "Francais");
+                
+                userconv = choiADj;
+                userconv.Visible = true;
+                choiADj.Show();
+                choiADj.BringToFront();
+                this.Controls.Add(choiADj); 
+                choiADj.Dock = DockStyle.Fill;
+                pictureBox1.BringToFront();
+            }
+            else
+            {
+                
+                Cours cf = new Cours();
+                cf.conjugaison = false;
+                cf.debut = 7;
+                cf.fin = 15;
+                cf.Show();
+
+            }
+        }
+        Bitmap b = new Bitmap(Application.StartupPath + @"/Pics/Francais/BackPro.jpg");
+
+        private void panel2_VisibleChanged(object sender, EventArgs e)
+        {
+            this.BackgroundImage= b;
+        }
+        int scoreTotal1,scoreTotal2;DataRow []dr;
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++) scoreTotal1 += scores[i];
+            
+            for (int i = 0; i < 5; i++) scoreTotal2 += scores[i];
+            
+            if (scoreTotal1 > int.Parse(dr[0]["SingulierOuPluriel"].ToString()))
+            {
+                dr[0]["SingulierOuPluriel"] = scoreTotal1;
+                Variables.XmlWriter(Application.StartupPath + "\\users.xml");
+            }
+            if (scoreTotal2 > int.Parse(dr[0]["Grammaire21"].ToString()))
+            {
+                dr[0]["Grammaire21"] = scoreTotal2;
+                Variables.XmlWriter(Application.StartupPath + "\\users.xml");
+            }
+            this.Close();
+            Variables.matiere.ShowInTaskbar = true;
+            Variables.matiere.Show();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            CryptageEtHachage.HashXmlUsers(Variables.UserNom, Variables.UserPass, Application.StartupPath + "\\users.xml");
+            Application.Exit();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            panconv.Visible = true;
+            userconv.Hide(); 
+            this.BackgroundImage = b;
+            pictureBox1.Visible = false;
+        }
+
+        
+    }
+}
---
+++ choixMultiple.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Start  
+{
+    public partial class choixMultiple : UserControl
+    {
+        public choixMultiple()
+        {
+            
+        }
+        public choixMultiple(ArrayList tabRandom, string chapitre, Panel p, int length, int maximum, string FichierXml)
+        {  
+            InitializeComponent();
+            rands = tabRandom;
+            lecon = chapitre;
+            pan = p; 
+            len = length;
+            max = maximum; 
+            xmlFile = FichierXml;
+           
+        }
+        XmlDocument gram; ArrayList randomss = new ArrayList(), rands; string choisi, lecon;int i = 0, score = 0; static  int len, max; Panel pan;
+
+        SoundPlayer truee =new SoundPlayer( @"Voix\trueAns1.wav"), wrong =new SoundPlayer( @"Voix\wrong.wav");
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            choisi = b.Text;
+                
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            if (choisi == gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[i] * 3]) { score += 5; truee.Play(); lblarr[i].BackColor = Color.Green; } else { wrong.Play(); lblarr[i].BackColor = Color.Red; }
+            label2.Text = "Score: " + score;
+            i++; if (i == len)
+            {
+               
+                DialogResult rep = MessageBox.Show(score/5 +   "  Reponses correctes. Voulez vous continuez?", lecon, MessageBoxButtons.YesNo, MessageBoxIcon.Question); if (rep == DialogResult.No)
+                {
+                    pan.Visible = true; this.Dispose();
+                    switch(lecon )
+                    {
+                        case "Adjectifs": cours_de_grammaire.scores[3] = score; break;
+                        case "Posses": cours_de_grammaire.scores[6] = score; break;
+                        default: break;
+                    }
+                    return;
+                } else
+                { 
+                    i = 0;
+                    rands = ComMethodes.Generate(len, max);
+                    score = 0;
+                    label2.Text = "Score: 0";
+                    foreach (RoundButton r in lblarr)
+                        r.BackColor = Color.Transparent;
+                }
+            }
+           label1.Text = gram.GetElementsByTagName(lecon)[0].InnerText.Split(',')[(int)rands[i]];
+            randomss = ComMethodes.Generate(3, 3);
+            b[(int)randomss[0]].Text = gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[i]*3];
+            b[(int)randomss[1]].Text = gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[i]*3+1];
+            b[(int)randomss[2]].Text = gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[i]*3+2];
+        }
+        Button[] b = new Button[3];
+        RoundButton[] lblarr;
+        string xmlFile;
+        private void choixMultiple_Load(object sender, EventArgs e)
+        {
+            lblarr = new RoundButton[len];
+            for (int i = 0; i < len ; i++)
+            {
+                lblarr[i] = new RoundButton(); lblarr[i].Size = new Size(30, 30);
+                lblarr[i].Location = new Point(13 + i * 30, 11);
+                panel1.Controls.Add(lblarr[i]);
+
+            }
+            gram = new XmlDocument();
+            if (xmlFile == "Francais")
+            {
+                gram.Load(Application.StartupPath + @"\Francais.xml");
+                CryptageEtHachage.DeCrypNode(gram.DocumentElement);
+            }
+            else
+            {
+                gram.Load(Application.StartupPath + @"\Prof.xml");
+            }
+            randomss = ComMethodes.Generate(3, 3);
+            for (int k = 0; k < 3; k++)
+            {
+                b[k] = new Button();
+                b[k].Size = new Size(200, 70); 
+                b[k].Location = new Point(100+200*k,280);
+                b[k].MouseEnter += changefont; 
+                b[k].BackColor = SystemColors.Info;
+                b[k].Font= new Font("Lemon", 10, FontStyle.Regular);
+                this.Controls.Add(b[k]);
+                b[k].Click += button2_Click;
+            }
+          
+            label1.Text = gram.GetElementsByTagName(lecon)[0].InnerText.Split(',')[(int)rands[i]];
+
+            b[(int)randomss[0]].Text = gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[0] * 3];
+            b[(int)randomss[1]].Text = gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[0] * 3+1];
+            b[(int)randomss[2]].Text = gram.GetElementsByTagName(lecon)[1].InnerText.Split(',')[(int)rands[0] * 3+2];
+
+        }
+
+        private void changefont(object sender, EventArgs e)
+        { 
+            Button b = new Button();
+            b.Font = new Font("Lemon", 14, FontStyle.Regular);
+        }
+    }
+}
